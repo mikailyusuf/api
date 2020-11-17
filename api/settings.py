167cb882af6f12c05abac51bc50cec5f0b18de9f
Mikail Yusuf <mikailkyusuf@gmail.com>
2020-11-17 14:21:22 +0100

    Deleted some files

--- api/settings.py
+++ api/settings.py
@@ -124,17 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-#S3 BUCKETS CONFIG
-AWS_ACCESS_KEY_ID = 'AKIAZRJ3FW6Q7DLCOWUT'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'mikailapi'
 
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = 'eu-west-2'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
